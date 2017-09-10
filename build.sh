3c08f4e62c80e78c0cf3c8c91dc526d153323334
webfolderio <support@webfolder.io>
2017-09-10 21:31:09 +0300

    minor fix

--- build.sh
+++ build.sh
@@ -1,7 +1,8 @@
+
 #!/bin/bash
 
 JAVA_HOME=/usr/lib/jvm/java-1.8.0-openjdk-amd64
 
 rm -f src/main/resources/META-INF/libfast-md5.so
-gcc src/main/c/md5-linux.c src/main/c/md5-jni.c -std=c99 -Wall -fPIC -lcrypto -lssl -O3 -Wno-return-type -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -shared -o src/main/resources/META-INF/libfast-md5.so
+gcc src/main/c/md5-linux.c -std=c99 -Wall -fPIC -O3 -lssl -lcrypto -shared -Wno-return-type -I$JAVA_HOME/include -I$JAVA_HOME/include/linux -o src/main/resources/META-INF/libfast-md5.so
 strip src/main/resources/META-INF/libfast-md5.so
